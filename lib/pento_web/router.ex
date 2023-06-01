22010b92d8efb95c780bc9b24fc3b3e491171f75
Juan Soler <soleronline@gmail.com>
2023-06-01 13:45:53 +0200

    Added Catalog and Product

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -22,6 +22,15 @@ defmodule PentoWeb.Router do
     pipe_through [:browser, :require_authenticated_user]
 
       live "/guess", WrongLive
+
+      live "/products", ProductLive.Index, :index
+      live "/products/new", ProductLive.Index, :new
+      live "/products/:id/edit", ProductLive.Index, :edit
+
+      live "/products/:id", ProductLive.Show, :show
+      live "/products/:id/show/edit", ProductLive.Show, :edit
+
+
     get "/users/settings", UserSettingsController, :edit
   end
 end
