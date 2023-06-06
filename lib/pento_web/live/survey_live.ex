c1bbe1b1ef1e0ae91d8ace8ea4c689497b8c9465
Juan Soler <soleronline@gmail.com>
2023-06-06 13:59:58 +0200

    Added Survey. Part 2

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -5,10 +5,16 @@ defmodule PentoWeb.SurveyLive do
   alias Pento.Survey
   alias PentoWeb.DemographicLive
   alias PentoWeb.DemographicLive.Form
+  alias PentoWeb.RatingLive
+  alias Pento.Catalog
   # alias PentoWeb.DemographicLive
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign_demographic}
+    {:ok,
+      socket
+      |> assign_demographic()
+      |> assign_products()
+    }
   end
 
   def assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
@@ -19,13 +25,31 @@ defmodule PentoWeb.SurveyLive do
     )
   end
 
+  def assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_rating(user)
+  end
+
   def handle_info({:created_demographic, demographic}, socket) do
     {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
+  def handle_info({:created_rating, update_product, product_index}, socket) do
+    {:noreply, handle_rating_created(socket, update_product, product_index)}
   end
 
   def handle_demographic_created(socket, demographic) do
     socket
     |> put_flash(:info, "Demographic created successfully")
     |> assign(:demographic, demographic)
+  end
+
+  def handle_rating_created(%{assigns: %{products: products}} = socket, update_product, product_index) do
+    socket
+    |> put_flash(:info, "Rating submitted successfully")
+    |> assign(:products, List.replace_at(products, product_index, update_product))
   end
 end
